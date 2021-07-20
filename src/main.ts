b345eb1e0b32c00178b13e436c1280a232363e5b
Xie Yuheng <xyheme@gmail.com>
2021-07-20 21:44:50 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue"
 import App from "./app.vue"
 import "./plugins/register-service-worker"
 import router from "./router"
+import "./styles/tailwind.css"
 
 createApp(App).use(router).mount("#app")
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Hello Expressions!</div>
+  <div class="font-bold">Hello Expressions!</div>
 </template>
 
 <script lang="ts">
