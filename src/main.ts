6cea4f37e8467bbf0ae68aba4049750d13fd47ac
Xie Yuheng <xyheme@gmail.com>
2021-07-20 21:46:16 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue"
 import App from "./app.vue"
-import "./plugins/register-service-worker"
 import router from "./router"
+import "./plugins/register-service-worker"
 import "./styles/tailwind.css"
 
 createApp(App).use(router).mount("#app")
