bd91937447f3eedb8521a01fdb9378c33a782463
Xie Yuheng <xyheme@gmail.com>
2021-07-20 21:35:15 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
-import App from "./App.vue"
-import "./registerServiceWorker"
+import App from "./app.vue"
+import "./plugins/register-service-worker"
 import router from "./router"
 
 createApp(App).use(router).mount("#app")
