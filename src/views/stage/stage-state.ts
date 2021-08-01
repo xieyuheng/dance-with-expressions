8bd78f82e6cbc824c1ad82adfcc37b65b48f6fbc
Xie Yuheng <xyheme@gmail.com>
2021-08-02 00:29:36 +0800

    up

--- src/views/stage/stage-state.ts
+++ src/views/stage/stage-state.ts
@@ -1,3 +1,7 @@
+import { GitLibrary } from "@cicada-lang/librarian"
+import { GitLabLibrary } from "@cicada-lang/librarian/lib/git-libraries"
+import { GitHubLibrary } from "@cicada-lang/librarian/lib/git-libraries"
+
 export class StageState {
   static async build(): Promise<StageState> {
     return new StageState()
